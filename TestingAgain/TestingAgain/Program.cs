bf884c270e824a795f97357873efc27ec70342b0
Phyu Thet <phyumyeon@gmail.com>
2023-03-03 11:07:14 +0630

    Testing Two

---
+++ TestingAgain/TestingAgain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestingAgain;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(3+5);
+    }
+}
+
