9cf88042d960b5f176c1e5f55bf3951fa60dc252
PhoneZaw <phonezawuit@gmail.com>
2023-03-13 11:49:23 +0630

    add pz

--- Array-Challenges/Program.cs
+++ Array-Challenges/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Array_Challenges
 {
@@ -6,7 +7,28 @@ namespace Array_Challenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Input Your Numbers Array: ");
+            int[] ary = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+
+            Console.Write("Input a number to search and delete: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Your Array => " + string.Join(" ", ary) + "\n");
+
+            Console.WriteLine("Sum of Numbers: " + ary.Sum());
+
+            Console.WriteLine("Count of Negative Numbers: " + ary.Count(x => x < 0));
+
+            Console.WriteLine("Max Number: " + ary.Max() + " \nMin Number: " + ary.Min());
+
+            Console.WriteLine("Count of Even: " + ary.Count(x => x % 2 == 0) + " \nCount of Odd: " + ary.Count(x => x % 2 != 0));
+
+            Console.WriteLine("Reverse Ary: " + string.Join(" ", ary.Reverse()));
+
+            Console.WriteLine($"Search {num}: {ary.Contains(num)} ");
+
+            Console.WriteLine($"Delete {num}: {string.Join(" ", ary.Where(x => x != num))} ");
         }
     }
 }
