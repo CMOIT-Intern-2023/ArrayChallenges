b52bbc123328abe4ea5fb248f0db1cc9b367a627
nawehhser <127178957+nawehhser@users.noreply.github.com>
2023-03-15 11:48:48 +0630

    updated

--- arrayexercise/arrayexercise/Program.cs
+++ arrayexercise/arrayexercise/Program.cs
@@ -8,7 +8,7 @@ namespace arrayexercise
     {
         static void Main(string[] args)
         {
-            int i, sum = 0;
+             int i, sum = 0;
             Console.WriteLine("Enter 9 numbers");
 
             int[] num = new int[9];
@@ -30,25 +30,53 @@ namespace arrayexercise
             }
             Console.WriteLine("The sum of the elements in the array is:" + sum);
             var neg = num.Count(num => num < 0);
+
             Console.WriteLine("The numbers of negative in the array is " + neg);
-            Console.WriteLine("The maximum element is" + num.Max());
-            Console.WriteLine("The minumum element is" + num.Min());
+
+            Console.WriteLine("The maximum element is :" + num.Max());
+
+            Console.WriteLine("The minumum element is :" + num.Min());
+
             var even = num.Count(num => num % 2 == 0);
-            Console.WriteLine("The numbers of even numbers in the array are " + even);
+            Console.WriteLine("The numbers of even numbers in the array are : " + even);
+
             var odd = num.Count(num => num % 2 != 0);
-            Console.WriteLine("The numbers of odd numbers in the array are " + odd);
+            Console.WriteLine("The numbers of odd numbers in the array are : " + odd);
 
             num.Reverse();
             foreach (var items in num)
                 Console.WriteLine(items);
+
             Console.WriteLine("Search the number");
             Console.WriteLine(num.Contains(Convert.ToInt32(Console.ReadLine())));
-            List<int> list = num.ToList<int>();
-            Console.WriteLine("Enter the number you want to remove");
-            list.Remove(Convert.ToInt32(Console.ReadLine()));
-            foreach (var items in list)
-                Console.WriteLine(items);
 
+
+            List<int> list = num.ToList<int>();
+
+           
+                Console.WriteLine("Enter the number you want to remove");
+
+            bool check = true;
+            while (check)
+            {
+                if (list.Remove(Convert.ToInt32(Console.ReadLine())))
+                {
+                    Console.WriteLine("After removing the number");
+                    foreach (var items in list)
+                        Console.WriteLine(items);
+                }
+                else
+                {
+                    Console.WriteLine("The number does not exist");
+                    Console.Write("Do you want to remove another number? ");
+                    string removeNum = Console.ReadLine();
+                    check = removeNum == "yes" ? true : false;
+                    Console.WriteLine("Enter the number you want to remove");
+
+
+                }
+                 
+                }
 
         }
     }
