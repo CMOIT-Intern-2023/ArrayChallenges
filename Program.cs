61ebc159f91df28dc08272e70f086664979d9612
Yamete <ymt.yemyattun@gmail.com>
2023-03-15 10:45:49 +0630

    update 2

--- Program.cs
+++ Program.cs
@@ -30,56 +30,43 @@ class FirstChallenge
         }
 
         Console.WriteLine("--------------------------------");
-        Console.WriteLine("Your entered these numbers:");
-        Console.WriteLine(string.Join(", ", numbers));
+        Console.WriteLine("Your entered these numbers: " + string.Join(", ", numbers));
 
         var negNum = numbers.Where(n => n < 0).ToList();
-        Console.WriteLine("--------------------------------");
-        Console.WriteLine("Negative numbers are:");
-        Console.WriteLine(string.Join(", ", negNum));
+        Console.WriteLine("\nNegative numbers are: " + string.Join(", ", negNum));
 
         var evenNum = numbers.Where(n => n % 2 == 0).ToList();
         var oddNum = numbers.Where(n => n % 2 != 0).ToList();
-        Console.WriteLine("--------------------------------");
-        Console.WriteLine($"Count of even numbers is {evenNum.Count}");
+        Console.WriteLine($"\nCount of even numbers is {evenNum.Count}");
         Console.WriteLine($"Count of odd numbers is {oddNum.Count}");
 
-        Console.WriteLine("--------------------------------");
-        Console.WriteLine($"Sum of all numbers is {numbers.Sum()}");
+        Console.WriteLine($"\nSum of all numbers is {numbers.Sum()}");
 
-        Console.WriteLine("--------------------------------");
-        Console.WriteLine($"Count of negative numbers is {negNum.Count}");
+        Console.WriteLine($"\nCount of negative numbers is {negNum.Count}");
 
-        Console.WriteLine("--------------------------------");
-        Console.WriteLine($"Maximum number is {numbers.Max()}");
+        Console.WriteLine($"\nMaximum number is {numbers.Max()}");
         Console.WriteLine($"Minimum number is {numbers.Min()}");
 
-        Console.WriteLine("--------------------------------");
-        Console.WriteLine("Reverse array is:");
         int[] reversedNumbers = new int[totalNumbers];
         numbers.CopyTo(reversedNumbers, 0);
         Array.Reverse(reversedNumbers);
-        Console.WriteLine(string.Join(", ", reversedNumbers));
+        Console.WriteLine("\nReverse array is: " + string.Join(", ", reversedNumbers));
 
-        Console.WriteLine("--------------------------------");
-        Console.WriteLine("What number do you want to search?");
+        Console.WriteLine("\nWhat number do you want to search?");
         if (!int.TryParse(Console.ReadLine(), out int target))
         {
             Console.WriteLine("Invalid input. Please enter a valid integer.");
             return;
         }
-
         bool isExist = numbers.Contains(target);
         Console.WriteLine(isExist ? "Element found in the array." : "Element not found in the given array.");
 
-        Console.WriteLine("--------------------------------");
-        Console.WriteLine("What number do you want to delete?");
+        Console.WriteLine("\nWhat number do you want to delete?");
         if (!int.TryParse(Console.ReadLine(), out target))
         {
             Console.WriteLine("Invalid input. Please enter a valid integer.");
             return;
         }
-
         if (numbers.Contains(target))
         {
             var numbersAfterDeletion = numbers.ToList();
@@ -87,10 +74,7 @@ class FirstChallenge
             Console.WriteLine($"Element {target} deleted.");
             Console.WriteLine($"New array is: {string.Join(", ", numbersAfterDeletion)}");
         }
-        else
-        {
-            Console.WriteLine("Element not found in the given array.");
-        }
+        else Console.WriteLine("Element not found in the given array.");
 
         Console.WriteLine("--------------------------------");
     }
