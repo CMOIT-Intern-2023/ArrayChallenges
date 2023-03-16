2050d5780f9b57e24476257aca4804771c99ac1c
thethmue <thethmue260@gmail.com>
2023-03-16 11:55:35 +0630

    ver1

--- Students/Program.cs
+++ Students/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 namespace ConsoleApp1
 {
@@ -12,7 +13,7 @@ namespace ConsoleApp1
             while (!Double.TryParse(input, out filtered_input))
             {
                 Console.WriteLine("Failed. You can only input integers.");
-                Console.Write("Enter number again: ");
+                Console.Write("\nEnter number again: ");
                 input = Console.ReadLine();
             }
 
@@ -25,78 +26,108 @@ namespace ConsoleApp1
             bool flag = true;
             do
             {
-                Console.WriteLine("Enter numbers of inputs: ");
-                int num = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Input number/s: ");
+                bool isD = true;
                 List<double> arr = new List<double>();
+                List<string> inputs = new List<string>();
                 double sum = 0;
-                string input;
-                double filtered_input;
-                for (int i = 0; i < num; i++)
-                {
-                    input = Console.ReadLine();
-                    filtered_input = ValidateInput(input);
-                    arr.Add(filtered_input);
-                    sum += filtered_input;
 
+                while (isD)
+                {
+
+                    Console.Write("Input number/s with space between them: ");
+                    string input = Console.ReadLine();
+                    string[] subs = input.Split(' ');
+
+                    try
+                    {
+                        double[] arr1 = Array.ConvertAll(subs, Double.Parse);
+                        foreach (double s in arr1)
+                        {
+                            arr.Add(s);
+                            sum += s;
+                        }
+
+                        isD = false;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Failed. Enter again! \n");
+
+
+                    }
                 }
 
                 int c = arr.Count(x => x < 0);
                 int even = arr.Count(x => x % 2 == 0);
                 int odd = arr.Count(x => x % 2 != 0);
-                Console.WriteLine("Sum: " + sum);
-                Console.WriteLine("Count of negative numbers: " + c);
-                Console.WriteLine("Min :" + arr.Min());
-                Console.WriteLine("Max :" + arr.Max());
-                Console.WriteLine("Count of even numbers: " + even);
-                Console.WriteLine("Count of odd numbers: " + odd);
+                Console.WriteLine("\nSum: " + sum);
+                Console.WriteLine("\nCount of negative numbers: " + c);
+                Console.WriteLine("\nMin :" + arr.Min());
+                Console.WriteLine("\nMax :" + arr.Max());
+                Console.WriteLine("\nCount of even numbers: " + even);
+                Console.WriteLine("\nCount of odd numbers: " + odd);
                 arr.Reverse();
-                Console.Write("Reverse: ");
+                Console.Write("\nReverse: ");
 
                 foreach (double a in arr)
                 {
                     Console.Write(a);
                     Console.Write(" ");
-                }
 
-                Console.Write("\nSearch a number: ");
-                string search = Console.ReadLine();
-                double filtered_search = ValidateInput(search);
-                if (arr.Contains(filtered_search))
-                {
-                    Console.WriteLine("Found");
                 }
-                else
-                {
-                    Console.WriteLine("Not Found");
-                }
+                Console.WriteLine("");
 
-                Console.Write("Enter number you want to delete: ");
-                string del = Console.ReadLine();
-                double filtered_del = ValidateInput(del);
-                if (arr.Contains(filtered_del))
+                bool isFound = true;
+
+                do
                 {
-                    arr.Remove(filtered_del);
-                    Console.Write("Deleted! Numbers left: ");
-                    foreach (double a in arr)
+                    Console.Write("\nSearch a number: ");
+                    string search = Console.ReadLine();
+                    double filtered_search = ValidateInput(search);
+                    if (arr.Contains(filtered_search))
                     {
-                        Console.Write(a);
-                        Console.Write(" ");
+                        Console.WriteLine("Found");
+                        break;
                     }
-                }
-                else
-                {
-                    Console.WriteLine("Number not found in the list.");
-                }
+                    else
+                    {
+                        Console.WriteLine("Not Found. Enter again! \n");
+                        isFound = false;
+                    }
+                } while (!isFound);
 
-                Console.WriteLine("\nDo you want to start again? (y/n)");
+                do
+                {
+                    Console.Write("\nEnter number you want to delete: ");
+                    string del = Console.ReadLine();
+                    double filtered_del = ValidateInput(del);
+                    if (arr.Contains(filtered_del))
+                    {
+                        arr.Remove(filtered_del);
+                        Console.Write("Deleted! Numbers left: ");
+                        foreach (double a in arr)
+                        {
+                            Console.Write(a);
+                            Console.Write(" ");
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Found. Enter again!\n ");
+                        isFound = false;
+                    }
+                } while (!isFound);
+
+                Console.Write("\n\nDo you want to start again? (y/n): ");
 
                 string decision = Console.ReadLine();
+                Console.WriteLine("");
                 if (decision.Equals('n'))
                 {
                     flag = false;
                 }
-            } while (true);
+            } while (!flag);
         }
     }
 }
